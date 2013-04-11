b4607836efbc6777b1c39172ea43782f6fa4a1c4
Arnost Valicek <arnost.valicek@gmail.com>
2013-04-11 22:18:00 +0200

    mark ignored line with dot ( . )

--- src/main/java/com/github/arnostv/xquery/coverage/saxon/CoverageReport.java
+++ src/main/java/com/github/arnostv/xquery/coverage/saxon/CoverageReport.java
@@ -35,6 +35,37 @@ public class CoverageReport {
         return Collections.unmodifiableSet(unusedLines);
     }
 
+    public Set<Integer> ignoredLines() {
+        Set<Integer> ignoredLines = new HashSet<Integer>();
+
+        for (int i = 1, l = lines.size(); i <= l; i++) {
+            if (!coveredLines.contains(i) && isIgnoredLine(lines.get(i-1))) {
+                ignoredLines.add(i);
+            }
+        }
+
+        return Collections.unmodifiableSet(ignoredLines);
+
+    }
+
+    private boolean isIgnoredLine(final String line) {
+      if (line.isEmpty()) {
+          return true;
+      } else if (line.startsWith("import module")) {
+          return true;
+      } else if (line.startsWith("module namespace")) {
+          return true;
+      } else if (line.startsWith("import module")) {
+          return true;
+      } else if (line.startsWith("declare default")) {
+          return true;
+      } else if (line.trim().equals("};")) {
+          return true;
+      } else {
+          return false;
+      }
+    };
+
     public String lineText(int i) {
         return lines.get(i - 1);
     }
--- src/main/java/com/github/arnostv/xquery/coverage/saxon/ReportPlaintextRenderer.java
+++ src/main/java/com/github/arnostv/xquery/coverage/saxon/ReportPlaintextRenderer.java
@@ -7,11 +7,14 @@ public class ReportPlaintextRenderer {
         StringBuilder sb = new StringBuilder();
 
         final Set<Integer> coveredLines = report.coveredLines();
+        final Set<Integer> ignoredLines = report.ignoredLines();
 
         for (int i = 1, l = report.numberOfLines(); i <= l; i++) {
             sb.append(String.format("%4d ", i));
             if (coveredLines.contains(i)) {
                 sb.append("+ ");
+            } if (ignoredLines.contains(i)) {
+                sb.append(". ");
             } else {
                 sb.append("  ");
             }
